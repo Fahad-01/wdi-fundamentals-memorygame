244f27cd17b5d7b8fe363d49e1396ce98944f79d
fahad@fahad <fahad@fahad.com>
2019-11-11 09:32:54 -0500

    added game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,12 @@
+
+
+console.log("Up and running!");
+
+
+var cardOne = "King";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "Queen";
+
+console.log("usedFlipped", cardOne);
+console.log("usedFlipped", cardThree);
