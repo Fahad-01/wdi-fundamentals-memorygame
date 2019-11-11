d8eafeea6891f62bcf56aa4a30a98075e2b71aa4
fahad@fahad <fahad@fahad.com>
2019-11-11 14:54:42 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,10 +3,28 @@
 console.log("Up and running!");
 
 
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
 
-console.log("usedFlipped", cardOne);
-console.log("usedFlipped", cardThree);
+
+
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+
+
+console.log('User flipped', cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+
+console.log('User flipped', cardTwo);
+//console.log(cardsInPlay);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert('You found a match!');
+	} else{
+		alert("That's a MISS \nTry Again!!");
+	}
+}
